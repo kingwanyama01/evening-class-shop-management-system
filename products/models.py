48b3a1a191ee0f4d19c9fc2cffcc958ffd742548
kingwanyama01 <kingwanyama01@gmail.com>
2025-11-13 20:55:25 +0300

    Implimented product saving

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField()
+    quantity = models.CharField()
+    size = models.CharField()
+    price = models.CharField()
+    
+    def __str__(self):
+        return self.name
+    
--- products/views.py
+++ products/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Product
 
 def all_products(request):
     return render(request, 'products/index.html')
 
 def create(request):
-    
+    # Check if the request method is POST
+    if request.method == "POST":
+        # Start receiving data from the frontend
+        product_name = request.POST.get("p-name")
+        product_quantity = request.POST.get("p-qtty")
+        product_size = request.POST.get("p-size")
+        product_price = request.POST.get("p-price")
+        
+        # Use the data received
+        product = Product(name = product_name, quantity = product_quantity, size = product_size, price = product_price)
+        product.save()
+        
+        messages.success(request,"Data saved successfully!")
+        return redirect("create-products-url")        
+        
     return render(request,'products/create.html')
 
 def details(request):
